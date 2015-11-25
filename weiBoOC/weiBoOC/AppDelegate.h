7651d2440777d585a40e8b9a4b29f2f9fba4a8ab
wangwugang1314 <415461850@qq.com>
2015-11-25 22:29:42 +0800

    创建工程

---
+++ weiBoOC/weiBoOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weiBoOC
+//
+//  Created by MAC on 15/11/25.
+//  Copyright © 2015年 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weiBoOC/weiBoOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weiBoOC
+//
+//  Created by MAC on 15/11/25.
+//  Copyright © 2015年 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
